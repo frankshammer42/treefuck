205639380e23fddf20464474addd989c8619c5f8
ruian2 <ruian2@illinois.edu>
2019-09-30 18:36:23 -0400

    python2.7 support

--- treefuck.py
+++ treefuck.py
@@ -1,18 +1,12 @@
-#-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.
- #File Name : treefuck.py
- #Creation Date : 30-09-2019
- #Created By : Rui An  
-#_._._._._._._._._._._._._._._._._._._._._.
-import time
 yes_counter = 0
 no_counter = 0
 while(True):
+    print("Do trees behind you care?, yes or no?")
     print("Do trees in front you care?, yes or no?")
-    result = input() 
+    result = raw_input() 
     if (result == "yes"):
         yes_counter += 1
     elif(result == "no"):
         no_counter += 1
-    print("This many yes", yes_counter)
-    print("This many no", no_counter)
-    time.sleep(2)
+    print "This many yes", yes_counter
+    print "This many no", no_counter
